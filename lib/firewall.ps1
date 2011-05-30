55907e081b1e6f2b2ec11a25c7c4f2d0b6e8fdc7
xiaodao <xiaodao@.(none)>
2011-05-30 11:17:15 +0800

    disable/enable firewall profiles

--- lib/firewall.ps1
+++ lib/firewall.ps1
@@ -1,14 +1,38 @@
-$FWprofileTypes= @{1="Domain"; 2="Private" ; 4="Public"}
+$FwprofileTypes= @{1="Domain"; 2="Private" ; 4="Public"}
 $FwAction      = @{1="Allow"; 0="Block"}
-$FWDirection   = @{1="Inbound"; 2="outbound"; "Inbound"=1;"outbound"=2} 
+$FwDirection   = @{1="Inbound"; 2="outbound"; "Inbound"=1;"outbound"=2} 
+$FwProtocols   = @{1="ICMPv4";2="IGMP";6="TCP";17="UDP";41="IPv6";43="IPv6Route"; 44="IPv6Frag";
+                  47="GRE"; 58="ICMPv6";59="IPv6NoNxt";60="IPv6Opts";112="VRRP"; 113="PGM";115="L2TP";
+                  "ICMPv4"=1;"IGMP"=2;"TCP"=6;"UDP"=17;"IPv6"=41;"IPv6Route"=43;"IPv6Frag"=44;"GRE"=47;
+                  "ICMPv6"=48;"IPv6NoNxt"=59;"IPv6Opts"=60;"VRRP"=112; "PGM"=113;"L2TP"=115}
+
+
+$fw=New-object -comObject HNetCfg.FwPolicy2
+				  
+Function Disable-All-Profiles {
+	@(1, 2, 4) | %{
+		$fw.FireWallEnabled($_) = $false
+	}
+}
+
+Function Enable-All-Profiles {
+	@(1, 2, 4) | %{
+		$fw.FireWallEnabled($_) = $true
+	}
+}
+
+Function Get-EnabledRules {
+	$fw.rules | Where-Object {$_.enabled} | Sort-Object -Property direction | `
+		Format-Table -Property @{label="Direction"; expression= {$FwDirection[$_.direction]}}, name, `
+			@{Label="Action"; expression={$FwAction[$_.action]}}, @{Label="Protocol"; expression= {$FwProtocols[$_.protocol]}},`
+			localPorts -Autosize -wrap 
+}
 
 Function Get-FirewallConfig {
-	$fw=New-object -comObject HNetCfg.FwPolicy2
-
 	"Active Profiles(s) :" + (Convert-fwprofileType $fw.CurrentProfileTypes)
 
 	@(1,2,4) | select `
-                  @{Name="Network Type"     ;expression={$fwProfileTypes[$_]}},
+                  @{Name="Network Type"     ;expression={$FwprofileTypes[$_]}},
                   @{Name="Firewall Enabled" ;expression={$fw.FireWallEnabled($_)}},
                   @{Name="Block All Inbound";expression={$fw.BlockAllInboundTraffic($_)}},
                   @{name="Default In"       ;expression={$FwAction[$fw.DefaultInboundAction($_)]}},
@@ -18,8 +42,11 @@ Function Get-FirewallConfig {
 
 Function Convert-FWProfileType($profileCode){
 	$descriptions= @()
-	$FWprofileTypes.keys | %{
-			if ($profileCode -bAND $_) { $descriptions += $FWProfileTypes[$_] } 
-		}
+	$FwprofileTypes.keys | %{
+		if ($profileCode -bAND $_) { 
+			$descriptions += $FwprofileTypes[$_] 
+		} 
+	}
     $descriptions
 }
+
