1e77466b4c9cb9627b86a9fbe78bd5ccc3432bf4
xiaodao <xiaodao@.(none)>
2011-06-01 18:39:40 +0800

    add test

---
+++ test/string.test.ps1
@@ -0,0 +1,7 @@
+. c:\workspace\chef.net\lib\string.ps1
+
+Describe "Check string contain ignore case" {
+    it "check string contain ignore case" {
+	    (Contains-IgnoreCase "Test" "ES").should.be($true)
+	}
+}
