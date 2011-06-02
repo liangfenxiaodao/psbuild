58c04b7029ddf984b824312115270789b3d0aa2e
xiaodao <xiaodao@.(none)>
2011-06-02 10:13:13 +0800

    download firefox and install

--- lib/install_firefox.ps1
+++ lib/install_firefox.ps1
@@ -1,3 +1,5 @@
-Function Install-Firefox($firefoxPackage) {
-    iex "&'$firefoxPackage' -ms"
+Function Install-Firefox4 {
+	curl "http://mozilla.snt.utwente.nl//firefox/releases/4.0.1/win32/en-US/Firefox%20Setup%204.0.1.exe" -o "firefox.exe"
+    iex "& .\firefox.exe -ms"
+	iex "del .\$firefox.exe"
 }
