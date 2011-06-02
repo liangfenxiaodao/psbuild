a7f6c08a713bf559c1b5050612a7bfb14b8ff95d
xiaodao <xiaodao@.(none)>
2011-06-02 10:17:03 +0800

    download npp and install

--- lib/install_firefox.ps1
+++ lib/install_firefox.ps1
@@ -1,5 +1,5 @@
-Function Install-Firefox4 {
+Function Install-Firefox-4 {
 	curl "http://mozilla.snt.utwente.nl//firefox/releases/4.0.1/win32/en-US/Firefox%20Setup%204.0.1.exe" -o "firefox.exe"
-    iex "& .\firefox.exe -ms"
-	iex "del .\$firefox.exe"
-}
+    iex ".\firefox.exe -ms"
+	iex "del .\firefox.exe"
+}	
--- lib/install_notepad_plusplus.ps1
+++ lib/install_notepad_plusplus.ps1
@@ -1,3 +1,5 @@
-Function Install-NPP($nppPackage) {
-    iex "$nppPackage /S"
+Function Install-NPP-591 {
+	curl "http://download.tuxfamily.org/notepadplus/5.9.1/npp.5.9.1.Installer.exe" -o "npp.exe"
+    iex ".\npp.exe /S"
+	iex "del .\npp.exe"
 }
