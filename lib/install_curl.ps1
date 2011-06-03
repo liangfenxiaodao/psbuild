28863783c43a8aaba93dbb56a3150943af079678
xiaodao <xiaodao@.(none)>
2011-06-03 17:58:47 +0800

    put curl into repository

--- lib/install_curl.ps1
+++ lib/install_curl.ps1
@@ -3,10 +3,22 @@ $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 . $scriptDir\environment.ps1
 . $scriptDir\unblock_file.ps1
 
-Function Install-Curl($curlZipFile, $installToDir) {
-	Execute-If-Path-Not-Contains "curl" {
-		Unzip $curlZipFile $installToDir
-		Add-To-Path "$installToDir;"
-		Unblock-File "$installToDir\curl.exe"
+Function Install-Curl($installToDir) {
+	If(Curl-Installed) { 
+		Write-Host "Curl is already installed on this computer, will by pass Curl installation"
+		return 
 	}
+	
+	If(!(Test-Path $installToDir)) { New-Item $installToDir -type "Directory"}
+	Copy "$scriptDir\tools\curl.exe" $installToDir
+	Add-To-Path "$installToDir;"
+	Unblock-File "$installToDir\curl.exe"
+}
+
+Function Curl-Installed {
+	Try {
+		curl --version
+	}
+	Catch {}
+	$?
 }
