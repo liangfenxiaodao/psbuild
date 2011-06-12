05b67a3bca450edde43591f1698bdac4b068d665
Han Kai <isaachanstar@gmail.com>
2011-06-12 21:18:21 +0800

    add Set-WebAppPoolRuntimeVersion function

--- lib/webadministration_extensions.ps1
+++ lib/webadministration_extensions.ps1
@@ -22,3 +22,29 @@ function Set-WebAppPoolManagedPipelineMode {
     $modeValue = @{managedPipeLineMode="$Mode" }
     Set-WebConfiguration $filter -value $modeValue
 }
+
+function Set-WebAppPoolManagedRuntimeVersion {
+    param(
+       [Parameter(Mandatory=1)] $AppPoolName,
+       [Parameter(Mandatory=1)] $Version
+    )
+
+    $appPool = ls 'IIS:\AppPools' | ? { $_.Name -eq $AppPoolName }
+    if (!$appPool) { 
+        Write-Warning "App Pool $AppPoolName Not Exist"
+        return  
+    }
+    
+    if ($Version.ToLower() -eq "no managed") {
+        $Version = ""
+    }
+
+    if (($Version -ne "v2.0") -and ($Version -ne "v3.0") -and ($Version -ne "v4.0") -and ($Version -ne "")) {
+        Write-Warning "Runtime Version $Version is not supported."
+        return  
+    }
+       
+    $filter = '/system.applicationHost/applicationPools/add[@name="' + $AppPoolName + '"]'
+    $modeValue = @{managedRuntimeVersion="$Version" } 
+    Set-WebConfiguration $filter -value $modeValue
+}
