d7161c59333bc6763e203840d8ba514a217dd604
jacky li <veryfaint@gmail.com>
2011-06-14 14:46:15 +0800

    copy curl to module

--- module.ps1
+++ module.ps1
@@ -1,9 +1,9 @@
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 $moduleDir = "$root\ChefDotNet"
 
-Remove-Item $moduleDir -r #-ErrorAction SilentlyContinue
+Remove-Item $moduleDir -r -ErrorAction SilentlyContinue
 mkdir "$moduleDir"
 mkdir "$moduleDir\lib"
-cp "$root\lib\*" "$moduleDir\lib"
+cp -r "$root\lib\*" "$moduleDir\lib"
 cp *.psd1 $moduleDir
 cp *.psm1 $moduleDir
