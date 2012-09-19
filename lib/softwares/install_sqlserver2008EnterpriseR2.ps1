50ca563af466e879cebc58070d080fdb97aaf7ea
codekata-tw <codekata.tw@thoughtworks.com>
2012-09-19 16:01:20 +0800

    download sqlserver 2008 enterprise r2 install package.

--- lib/softwares/install_sqlserver2008EnterpriseR2.ps1
+++ lib/softwares/install_sqlserver2008EnterpriseR2.ps1
@@ -1,16 +1,18 @@
 $repo = "$home/.psbuild_repo"
 
-$installer = "sqlserver2008_enterprise_r2\setup.exe"
+$packageName = "sqlserver2008_enterprise_r2"
 
 Function Download-SqlServer2008EnterpriseR2 {
-	if (-not (Test-Path "$repo\$installer")) {
-		throw "Can't find sqlserver 2008 enterprise r2 installer $installer."
+	if (Test-Path "$repo\$packageName") {
+		return
 	}
+	Write-Host "Downloading SQLServer 2008 Enterprise R2 isntall package"
+	cp -recurse "\\10.18.1.16\Share\Microsoft\SQLServer\$packageName" "$repo"
 }
 
 Function Silent-Install-SqlServer2008EnterpriseR2 {
 	$configFile = "$scriptDir\softwares\sqlserverconfig\InstallConfig-2008EnterpriseR2.ini"
-	iex "$repo\$installer /ConfigurationFile=$configFile /IAcceptSQLServerLicenseTerms=True"
+	iex "$repo\$packageName\setup.exe /ConfigurationFile=$configFile /IAcceptSQLServerLicenseTerms=True"
 }
 
 Function SqlServer2008EnterpriseR2-Installation-Completed {
--- module.ps1
+++ module.ps1
@@ -1,9 +1,12 @@
 $root = Split-Path -Parent $MyInvocation.MyCommand.Path
 $moduleDir = "$root\ChefDotNet"
 
-rm $moduleDir -Recurse -Force
-mkdir "$moduleDir"
-mkdir "$moduleDir\lib"
+if (Test-Path $moduleDir) { 
+	rm $moduleDir -Recurse -Force 
+	mkdir "$moduleDir"
+	mkdir "$moduleDir\lib"
+}
+
 cp -r "$root\lib\*" "$moduleDir\lib"
 cp *.psd1 $moduleDir
 cp *.psm1 $moduleDir
